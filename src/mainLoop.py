4497505fe1669aad8b3b3f62887163c0ef1e0444
Damiano Racic <damianoracic06@gmail.com>
2026-07-13 15:18:30 -0700

    ball physics

--- src/mainLoop.py
+++ src/mainLoop.py
@@ -1,18 +1,26 @@
 import pygame
-# pygame setup
+import random
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-movSpeed = 5
+movSpeedPad = 5
+
+
+movSpeedBallx = -200
+movSpeedBally = random.randint(-1,1)*200
+
+radius = 5
 dt = 0
 
-player_pos = 200
-paddle = pygame.Rect(75, player_pos, 10, 75)
+player_pos = 320
+ball_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+
+
+paddle = pygame.Rect(75, player_pos, 10, 80)
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -21,16 +29,29 @@ while running:
     screen.fill("black")
 
     pygame.draw.rect(screen, "white", paddle, width=0)
+    pygame.draw.circle(screen, "white", ball_pos, radius, width=0)
 
     keys = pygame.key.get_pressed()
+    
+    
+    ball_pos.x += movSpeedBallx * dt
+    ball_pos.y += movSpeedBally * dt
+
+    if paddle.collidepoint(ball_pos.x - radius, ball_pos.y - radius):
+        movSpeedBallx *= -1
+    if ball_pos.y - radius <= 0 or ball_pos.y + radius >= screen.get_height():
+         movSpeedBally *= -1
+
     if keys[pygame.K_w] and player_pos > 0:
-        paddle.move_ip(0,-movSpeed)
-        player_pos -= movSpeed
+        paddle.move_ip(0,-movSpeedPad)
+        player_pos -= movSpeedPad
         print(player_pos)
+
     if keys[pygame.K_s] and player_pos < 650:
-        paddle.move_ip(0,movSpeed)
-        player_pos += movSpeed
+        paddle.move_ip(0,movSpeedPad)
+        player_pos += movSpeedPad
         print(player_pos)
+    
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -40,7 +61,7 @@ while running:
     # independent physics.
     dt = clock.tick(60) / 1000
 
-    if keys[pygame.K_q]:
+    if keys[pygame.K_q] or ball_pos.x - radius < 0 or ball_pos.x + radius > screen.get_width():
         running = False
 
 pygame.quit()
