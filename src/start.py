49f5f4b3793ef99a0819d1c40af723027ca1dec1
Damiano Racic <damianoracic06@gmail.com>
2026-07-22 16:03:16 -0700

    helpbutton added

--- src/start.py
+++ src/start.py
@@ -7,6 +7,7 @@ class Start:
     def __init__(self, screen):
         self.screen = screen
         self.startButton = pygame.Rect(screen.get_width()//2 - 200, screen.get_height()//2 - 150, 400,100)
+        self.helpButton = pygame.Rect(screen.get_width()//2 - 200, screen.get_height()//2 - 25, 400,100)
         self.font = pygame.freetype.Font(None, size=50)
     
     def startUI(self,events):
@@ -21,6 +22,9 @@ class Start:
 
         pygame.draw.rect(self.screen, "white", self.startButton, width=0)
         self.font.render_to(self.screen, (self.screen.get_width()//2 - 65, self.screen.get_height()//2 - 120), "Start", fgcolor=(0, 0, 0))
+
+        pygame.draw.rect(self.screen, "white", self.helpButton, width=0)
+        self.font.render_to(self.screen, (self.screen.get_width()//2 - 63, self.screen.get_height()//2 + 5), "Help", fgcolor=(0, 0, 0))
 
         pygame.display.flip()
 
